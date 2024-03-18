092781add6004ed1e8fc434356b2c59ddba5ed4e
Lalita Pawar <83364539+lalita-pawar@users.noreply.github.com>
2024-03-18 21:37:38 +0530

    Create  Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+import javafx.scene.shape.Rectangle;
+
+public class Paddle extends Rectangle {
+    private static final int WIDTH = 100;
+    private static final int HEIGHT = 20;
+
+    public Paddle() {
+        super(WIDTH, HEIGHT);
+        // Set paddle properties, such as color, position, etc.
+    }
+
+    // Add methods for paddle movement
+}
